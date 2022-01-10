a604d745007a3bd3feadd12aed1bfe5c8013f444
MeaninGood <jsmeaningood@gmail.com>
2022-01-10 23:50:09 +0900

    first.commit

---
+++ baekjoon/Bronze/1_1268해야함.py
@@ -0,0 +1,21 @@
+# 1268번_임시 반장 정하기
+
+## 1~5학년까지 한 번이라도 같은 반이었던 사람이 가장 많은 학생 임시 반장
+## 첫째 줄에 반의 학생 수를 나타내는 정수
+## 둘째 줄부터 1번 학생부터 차례대로 각 줄마다 1~5학년까지
+## 몇 반에 속했었는지 나타내는 5개의 정수 주어짐
+## 첫 줄에 임시 반장으로 정해진 학생 번호 출력
+## 단, 임시 반장 될 수 있는 학생 여러 명인 경우 가장 작은 번호 출력
+
+
+N = int(input())
+
+students = []
+for _ in range(N) :
+    each_class = list(map(int, input().split()))
+    students.append(each_class)
+
+cnt_class = []
+for i in range(N) :
+    for j in range(5) :
+        
