313b98bc9faf479c872c32ae1fe37b2cc6274174
MeaninGood <jsmeaningood@gmail.com>
2022-01-20 09:13:30 +0900

    first.commit

--- baekjoon/Silver/5_10814해야함.py
+++ baekjoon/Silver/5_10814해야함.py
@@ -18,7 +18,7 @@
 (출력)
 20 Sunyoung
 21 Junkyu
-21 Dohyun  
+21 Dohyun
 
 '''
 
@@ -31,8 +31,8 @@ B -> A 순으로 정렬
 
 '''
 
-from sys import stdin
-n = int(stdin.readline())
+# from sys import stdin
+# n = int(stdin.readline())
 
 '''리스트로 저장하니까 
 [<map object at 0x000002A87DF64E50>, <map object at 0x000002A87DF64DF0>]
@@ -43,4 +43,97 @@ for i in range(n) :
     li.append(list(map(str, stdin.readline().split())))
 
 '''
-print(li)
+# from sys import stdin
+# n = int(stdin.readline())
+
+# age = []
+# name = []
+# for i in range(n) :
+#     member = list(map(str, stdin.readline().split()))
+    # for j in range(1) :
+    #     a = int(name[i][j])
+    #     age.append(a)
+    #     b = name[i][j+1]
+    #     name.append(b) ### IndexError: list index out of range
+        
+        
+# print(name)
+# print(age)
+
+# dict_sort ={}
+# for _ in range(N):
+#   age, name = input().split()
+#   if dict_sort.get(age):
+#     dict_sort[age].append(name)
+#   else:
+#     dict_sort[age] = [name]
+    
+ 
+ 
+import sys
+N = int(sys.stdin.readline())
+
+    
+dict_sort = {}
+for i in range(N) :
+    age, name = sys.stdin.readline().split()
+    if dict_sort.get(name) :
+        dict_sort[name].append(age)
+    else :
+        dict_sort[name] = age
+
+result = sorted(dict_sort.items(), reverse = True)
+for i in range(N) :
+    print(result[i][1], result[i][0])
+
+
+# 딕셔너리로 받아서
+# 나이순으로만 정렬 -> 가입순 자동으로 맞춰짐
+
+
+
+
+# 나중에 lambda 를 활용한 정렬 방식도 한번 활용해보세용!
+
+#   if dict_sort.get(age):
+#         dict_sort[age].append(name)
+#   else:
+#     dict_sort[age] = [name]
+    
+# key값이 없으면 value 에 리스트형태로 대입
+# key 값이 있으면 value 에 append 해주는 형태로하면
+# 나이: 이름 으로 할 수 있어용!
+
+
+
+
+################## 0120 무조건 보기!!!!!!
+import sys
+
+N = int(sys.stdin.readline())
+
+dict_sort = {}
+for _ in range(N):
+    age, name = sys.stdin.readline().split()
+    if dict_sort.get(int(age)):
+        ## 키값이 있으면 append 로 value를 추가한다.
+        dict_sort[int(age)].append(name)
+    else:
+        ## 키값이 없으면 key:value 를 추가해 key값을 넣는다.
+        dict_sort[int(age)] = [name]
+result = sorted(dict_sort.keys())
+# print(dict_sort)
+for k in result:
+    tmp = dict_sort[k]
+    for t in tmp:
+        print(k,t)
+        
+        
+        
+N = int(input())
+arr = []
+for _ in range(N):
+    arr.append(input().split())
+arr.sort(key = lambda x: int(x[0]))
+for a in arr:
+    print(*a)
