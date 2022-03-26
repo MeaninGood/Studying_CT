62a681cc9e6be075504fb4ecfd342f7b78f6eeb6
Jiseon Jang <jsmeaningood@gmail.com>
2022-03-26 19:34:51 +0900

    22.03.26

---
+++ baekjoon/Gold/5_12851.py
@@ -0,0 +1,41 @@
+import sys
+from collections import deque
+si = sys.stdin.readline
+
+
+def bfs(s):
+    que = deque()
+    visited = [False for i in range(200010)]
+    cnt = 0
+    ret = 0
+    
+    que.append(s)
+    visited[s] = True
+    
+    while len(que) > 0:
+        size = len(que)
+
+        for _ in range(size):
+            cur = que.popleft()
+            
+            if cur == k:
+                ret += 1
+     
+            for nxt in [cur -1, cur + 1, 2 * cur]:
+                if not (0 <= nxt < 200010) or visited[nxt]:
+                    continue
+                
+                visited[nxt] = True
+                que.append(nxt)
+                
+        cnt += 1
+    visited[cur] = False
+    return cnt, ret
+
+n, k = map(int, si().split())
+
+v = [i for i in range(200010)]
+
+
+print(bfs(n))
+        
---
+++ baekjoon/Silver/1_11660.py
@@ -0,0 +1,18 @@
+import sys
+from pprint import pprint
+si = sys.stdin.readline
+
+n, m = map(int, si().split())
+arr = [[0 for _ in range(n + 1)]] + [[0] + list(map(int, si().split())) for _ in range(n)]
+prefix = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+pprint(arr)
+pprint(prefix)
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        prefix[i][j] = prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1] + arr[i][j]
+        
+for i in range(m):
+    x1, y1, x2, y2 = map(int, si().split())
+    
+    print(prefix[x2][y2] - prefix[x1 - 1][y2] - prefix[x2][y1 - 1] + prefix[x1 - 1][y1 - 1])
+    
