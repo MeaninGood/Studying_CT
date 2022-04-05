0247bc99fc1b3b7cdcb1ef6ef68e6e743f3ec22e
MeaninGood <jsmeaningood@gmail.com>
2022-04-05 14:16:15 +0900

    22.04.05

--- swea/0405/5248/sol.py
+++ swea/0405/5248/sol.py
@@ -25,3 +25,4 @@ for tc in range(1, T + 1):
             cnt += 1
             
     print(f'#{tc} {cnt}')
+
--- swea/0405/5249/sol.py
+++ swea/0405/5249/sol.py
@@ -1,3 +1,43 @@
+# def find_(x):
+#     if par[x] == x:
+#         return x
+    
+#     else:
+#         return find_(par[x])
+    
+# def union_(a, b):
+#     a = find_(a)
+#     b = find_(b)
+    
+#     if a == b:
+#         return
+    
+#     par[a] = b
+
+
+# T = int(input())
+# for tc in range(1, T + 1):
+#     n, m = map(int, input().split())
+#     v = [list(map(int, input().split())) for _ in range(m)]
+#     par = [i for i in range(n + 1)]
+
+#     v.sort(key = lambda x: x[2])
+
+#     ans = 0
+#     for i in range(m):
+#         x, y = v[i][0], v[i][1]
+        
+#         if find_(x) == find_(y):
+#             continue
+        
+#         union_(x, y)
+#         ans += v[i][2]
+        
+#     print(f'#{tc} {ans}')
+
+
+
+
 def find_(x):
     if par[x] == x:
         return x
@@ -13,24 +53,19 @@ def union_(a, b):
         return
     
     par[a] = b
+    
+n, m = map(int,  input().split())
+v = [list(map(int, input().split())) for _ in range(m)]
+par = [i for i in range(n + 1)]
 
+v.sort(key = lambda x: x[2])
 
-T = int(input())
-for tc in range(1, T + 1):
-    n, m = map(int, input().split())
-    v = [list(map(int, input().split())) for _ in range(m)]
-    par = [i for i in range(n + 1)]
-
-    v.sort(key = lambda x: x[2])
-
-    ans = 0
-    for i in range(m):
-        x, y = v[i][0], v[i][1]
-        
-        if find_(x) == find_(y):
-            continue
-        
-        union_(x, y)
-        ans += v[i][2]
-        
-    print(f'#{tc} {ans}')
+ans = 0
+for i in range(m):
+    x, y = v[i][0], v[i][1]
+    
+    if find_(x) == find_(y):
+        continue
+    
+    union_(x, y)
+    ans += v[i][2]
