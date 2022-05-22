515a41b6686fa4a0a900abe03bdd8b8d8c45d72b
Jiseon Jang <jsmeaningood@gmail.com>
2022-05-23 00:21:08 +0900

    22.05.21

--- swea/13941.py
+++ swea/13941.py
@@ -3,6 +3,7 @@ from collections import deque
 def in_range(x, y):
     return 0 <= x < n + 1 and 0 <= y < m + 1
 
+
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 def bfs(x, y):
@@ -38,10 +39,9 @@ def bfs(x, y):
         
     return -1
 
-visit = [False, False, False]
-tmp = [[], [], []]
 
-def recur(cur):
+visit = [False, False, False]
+def recur(cur, tmp):
     global idx
     if cur == 3:
         idx.append(tmp)
@@ -50,25 +50,25 @@ def recur(cur):
     for i in range(3):
         if visit[i]:
             continue
-        
-        tmp[i] = a[i]
         visit[i] = True
-        recur(cur + 1)
+        recur(cur + 1, tmp+[a[i]])
         visit[i] = False
+        
+        
 
 T = int(input())
 for tc in range(1, T + 1):
     n, m = map(int, input().split())
     arr = [['#' for _ in range(m + 1)]] + [(['#'] + list(input())) for _ in range(n)]
 
-    d = {'B': False, 'C': False, '_': False}
     a = [list(map(int, input().split())) for _ in range(3)]
     idx = []
     
-    recur(0)
+    recur(0, [])
 
     ans = 100000000
     for i in range(6):
+        d = {'B': False, 'C': False, '_': False}
         flag = False
         dist = 0
         for j in range(3):
