698547d01e03fca71eef8f68ba3fb062ea06d67f
MeaninGood <jsmeaningood@gmail.com>
2022-06-04 00:08:39 +0900

    22.06.04

--- programmers/lv2/17680.py
+++ programmers/lv2/17680.py
@@ -1,25 +1,24 @@
-size = 3
-ref = [4, 2, 3, 4, 5, 6, 5, 4, 7]
-che = []
-
-for r in ref:
-    if not r in che:
-        if len(che) < size:
-            che.append(r)
-            print('사이즈 작을 때')
-            print(che)
+def solution(cacheSize, cities):
+    answer = 0
+    
+    cache = []
+    
+    if cacheSize == 0:
+        return len(cities) * 5
+    
+    for ref in cities:
+        city = ref.lower()
+        if city not in cache:
+            if len(cache) < cacheSize:
+                cache.append(city)
+            else:
+                cache.pop(0)
+                cache.append(city)
+            answer += 5
+                
         else:
-            print('============')
-            che.pop(0)
-            print(che)
-            che.append(r)
-            print(che)
-            print('============')
-    else:
-        print('**********')
-        che.pop(che.index(r))
-        print(che)
-        che.append(r)
-        print(che)
-        print('**********')
-        
+            cache.pop(cache.index(city))
+            cache.append(city)
+            answer += 1
+    
+    return answer
