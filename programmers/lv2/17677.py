e1d8855ec8839b9ea8c145af1b17cd7de86743dd
Jiseon Jang <jsmeaningood@gmail.com>
2022-06-24 00:10:52 +0900

    22.06.23

--- programmers/lv2/17677.py
+++ programmers/lv2/17677.py
@@ -24,35 +24,8 @@
 #             tmp += str2[j].lower()
             
 #     if cnt == 2:
-#         arr2[tmp] = arr2.get(tmp, 0) + 1
-
-# print(arr1)
-# print(arr2)
-# # mn = {}
-# # mx = {}
-# # mntotal = 0
-# # mxtotal = 0
-# # for i in arr1:
-# #     if i in arr2:
-# #         mn[i] = min(arr1[i], arr2[i])
-# #         mntotal += min(arr1[i], arr2[i])
-# #         mx[i] = max(arr1[i], arr2[i])
-# #         mxtotal += max(arr1[i], arr2[i])
-# #         arr2.pop(i)
-# #     else:
-# #         mx[i] = 1
-# #         # mxtotal += arr2[i]
-
-# # print(mn)
-# # print(mx)
-
-# # print(f'{(mntotal / mxtotal) * 65536:.0f}')
-        
-
-# mxd = arr1
-
-# for i in arr2:
-#     mxd[i] = mxd.get(i, max(mxd[i], arr2[i])) + 1
     
-# print(mxd)
+# print(mx)
 
+str1 = "hahahaja"
+str2 = ""
