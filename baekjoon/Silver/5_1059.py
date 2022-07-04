e4c174adb7416675a8b7675566149804c00ae5b8
Jiseon Jang <jsmeaningood@gmail.com>
2022-07-05 04:09:48 +0900

    22.07.05

--- baekjoon/Silver/5_1059.py
+++ baekjoon/Silver/5_1059.py
@@ -1,52 +1,24 @@
-# n = int(input())
-# arr = list(map(int, input().split()))
-# k = int(input())
+import sys
+input = lambda : sys.stdin.readline().strip()
 
-
-# cnt = 0
-# for i in range(1, n):
-#     s = 0
-#     e = 0
-#     if arr[i - 1] <= k <= arr[i]:
-#         if k - arr[i - 1] > 1:
-#             for j in range(arr[i - 1] + 1, k + 1):
-#                 cnt += 1
-        
-#         if k - arr[i - 1] == 1:
-#             for j in range(k + 1, arr[i]):
-#                 cnt += 1
-        
-        
-
-        
-# print(cnt)
-
-
-# n = int(input())
-# arr = list(map(int, input().split()))
-# s = int(input())
-
-
-# cnt = 0
-# for i in range(1, n):
-#     if arr[i - 1] < s < arr[i]:
-#         for j in range(arr[i - 1] + 1, s + 1):
-#             for k in range(j + 1, arr[i]):
-#                 print(f'#{arr[i-1]} {arr[i]} {j} {k}')
-#                 cnt += 1
-
-        # for j in range(arr[i - 1] + 1, s):
-        #     cnt += 1
-
-        
-        # for j in range(s + 1, arr[i]):
-        #     cnt += 1
-        #     print(f'%{j}')
-# print(cnt)
 
 n = int(input())
-arr = [int(input()) for _ in range(n)]
-s = int(input())
+arr = list(map(int, input().split()))
+m = int(input())
 
-for i in range(n):
-    if arr[i] <= s <= 
+arr.append(0)
+arr.append(1001)
+
+arr.sort()
+
+ans = 0
+for k in range(1, n + 2):
+    cnt = 0
+    for i in range(arr[k - 1] + 1, arr[k]):
+        for j in range(arr[k - 1] + 1, arr[k]):
+            if i < j and (i <= m <= j):
+                cnt += 1
+
+    ans = max(ans, cnt)
+
+print(ans)
