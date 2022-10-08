eaede507cc77272348a31520034a6d72db64b026
JangJiseon <jsmeaningood@gmail.com>
2022-10-08 19:57:27 +0900

    22.10.08

--- baekjoon/Gold/4_20952.py
+++ baekjoon/Gold/4_20952.py
@@ -70,48 +70,110 @@
 # #                 idx += 1
 
 
+# import sys
+# input = lambda : sys.stdin.readline().strip()
+
+
+# n, m = map(int, input().split())
+# arr1 = list(map(int, input().split()))
+# prefix = list(map(int, input().split()))
+# arr = [0 for _ in range(7)]
+
+# for i in arr1:
+#     arr[i % 7] += 1
+
+# for i in range(m):
+#     idx = arr[(7 - prefix[i]) % 7]
+#     arr[(7 - prefix[i]) % 7] = 0
+#     tmp = arr[:]
+
+#     cnt = 0
+#     for j in range(7):
+#         if arr[j] == 0:
+#             cnt += 1
+            
+#     if cnt == 7:
+#         arr[(7 - prefix[i]) % 7] = idx
+#         continue
+        
+#     for j in range(7):
+#         arr[(j + prefix[i]) % 7] = tmp[j]
+
+# k = 0
+# for i in arr:
+#     if i != 0:
+#         k += i
+
+# if k == 0:
+#     print(n)
+#     print(*arr1)
+    
+# else:
+#     print(k)
+#     for i in range(n):
+#         # print(arr1)
+#         if arr[i % 7] != 0:
+#             print(arr1[i] + sum(prefix) % (10**9 + 7), end = ' ')
+#             arr[i % 7] -= 1
+
+
 import sys
 input = lambda : sys.stdin.readline().strip()
 
+"""
+n, m 제한 = 10만 (최대 n * n)
+완탐 다 돌면 10!이 나옴, 완탐 x
+"""
 
 n, m = map(int, input().split())
-arr1 = list(map(int, input().split()))
-prefix = list(map(int, input().split()))
-arr = [0 for _ in range(7)]
+arr = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+idx = [0 for _ in range(7)]
+mx = 0
+for i in arr:
+    idx[i % 7] += 1
+    if idx[i % 7] > mx:
+        mx = idx[i % 7]
 
-for i in arr1:
-    arr[i % 7] += 1
+zero = 0
+for i in range(7):
+    if idx[i] == 0:
+        zero += 1
+
+if zero == 6:
+    answer = []
+    for i in range(n):
+        answer.append(arr[i] % (10**9 + 7))
+
+    print(m)
+    print(*answer)
+
+    exit()
 
 for i in range(m):
-    idx = arr[(7 - prefix[i]) % 7]
-    arr[(7 - prefix[i]) % 7] = 0
-    tmp = arr[:]
+    tmp = idx[(7 - (arr2[i] % 7)) % 7]
+    idx[(7 - (arr2[i] % 7)) % 7] = 0
+    zero += 1
 
-    cnt = 0
-    for j in range(7):
-        if arr[j] == 0:
-            cnt += 1
-            
-    if cnt == 7:
-        arr[(7 - prefix[i]) % 7] = idx
-        continue
-        
-    for j in range(7):
-        arr[(j + prefix[i]) % 7] = tmp[j]
+    if zero == 6:
+        zero -= 1
+        idx[(7 - (arr2[i] % 7)) % 7] = tmp
 
-k = 0
-for i in arr:
-    if i != 0:
-        k += i
+    idx = idx[(7 - (arr2[i] % 7)) % 7:] + idx[:((7 - (arr2[i] % 7)) % 7)]
 
-if k == 0:
-    print(n)
-    print(*arr1)
-    
-else:
-    print(k)
-    for i in range(n):
-        print(arr1)
-        if arr[i % 7] != 0:
-            print(arr1[i] + sum(prefix), end = ' ')
-            arr[i % 7] -= 1
+print(idx)
+
+cnt = 0
+total = sum(arr2)
+answer = []
+for i in range(n* mx):
+    print(i // 7, ((i // 7) * 7) + (i % 7))
+    if idx[i % 7] != 0 and ((i // 7) * 7) + (i % 7) <= n:
+        idx[i % 7] -= 1
+        answer.append((arr[((i // 7) * 7) + (i % 7)] + total) % (10**9 + 7))
+        cnt += 1
+
+    print(idx)
+
+print(cnt)
+print(*answer)
