b35a5ca01469bed91a73edc92dcf70e5c3700abf
JangJiseon <jsmeaningood@gmail.com>
2022-10-09 21:34:32 +0900

    22.10.09

--- baekjoon/Gold/4_20952.py
+++ baekjoon/Gold/4_20952.py
@@ -117,63 +117,116 @@
 #             arr[i % 7] -= 1
 
 
-import sys
-input = lambda : sys.stdin.readline().strip()
+# import sys
+# input = lambda : sys.stdin.readline().strip()
 
 """
 n, m 제한 = 10만 (최대 n * n)
 완탐 다 돌면 10!이 나옴, 완탐 x
 """
 
+# n, m = map(int, input().split())
+# arr = list(map(int, input().split()))
+# arr2 = list(map(int, input().split()))
+# idx = [0 for _ in range(7)]
+# mx = 0
+# for i in arr:
+#     idx[i % 7] += 1
+#     if idx[i % 7] > mx:
+#         mx = idx[i % 7]
+
+# zero = 0
+# for i in range(7):
+#     if idx[i] == 0:
+#         zero += 1
+
+# if zero == 6:
+#     answer = []
+#     for i in range(n):
+#         answer.append(arr[i] % (10**9 + 7))
+
+#     print(m)
+#     print(*answer)
+
+#     exit()
+
+# for i in range(m):
+#     tmp = idx[(7 - (arr2[i] % 7)) % 7]
+#     tmp2 = idx[:]
+
+#     idx = idx[(7 - (arr2[i] % 7)) % 7:] + idx[:((7 - (arr2[i] % 7)) % 7)]
+#     idx[(7 - (arr2[i] % 7)) % 7] = 0
+#     zero += 1
+
+#     if zero == 6:
+#         zero -= 1
+#         idx[(7 - (arr2[i] % 7)) % 7] = tmp
+#         idx = tmp2
+
+
+# print(idx)
+
+# cnt = 0
+# total = sum(arr2)
+# answer = []
+# for i in range(n* mx):
+#     print(i // 7, ((i // 7) * 7) + (i % 7))
+#     if idx[i % 7] != 0 and ((i // 7) * 7) + (i % 7) <= n:
+#         idx[i % 7] -= 1
+#         answer.append((arr[((i // 7) * 7) + (i % 7)] + total) % (10**9 + 7))
+#         cnt += 1
+
+#     print(idx)
+
+# print(cnt)
+# print(*answer)
+
+
+import sys
+input = lambda : sys.stdin.readline().strip()
+
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 arr2 = list(map(int, input().split()))
 idx = [0 for _ in range(7)]
-mx = 0
+
 for i in arr:
     idx[i % 7] += 1
-    if idx[i % 7] > mx:
-        mx = idx[i % 7]
 
 zero = 0
-for i in range(7):
-    if idx[i] == 0:
+for i in idx:
+    if i == 0:
         zero += 1
 
-if zero == 6:
-    answer = []
-    for i in range(n):
-        answer.append(arr[i] % (10**9 + 7))
+answer = []
 
-    print(m)
-    print(*answer)
-
-    exit()
+prefix = [0 for i in range(m)]
+prefix[0] = arr2[0] % (10**9 + 7)
+for i in range(1, m):
+    prefix[i] = prefix[i - 1] + arr2[i]
 
 for i in range(m):
-    tmp = idx[(7 - (arr2[i] % 7)) % 7]
-    idx[(7 - (arr2[i] % 7)) % 7] = 0
-    zero += 1
+    if idx[(7 - ((prefix[i] % 7))) % 7] != 0:
+        tmp = idx[(7 - ((prefix[i] % 7))) % 7] # 값 저장
+        idx[(7 - ((prefix[i] % 7))) % 7] = 0 # 바꾸기
+        zero += 1 # 0 개수 += 1
 
-    if zero == 6:
-        zero -= 1
-        idx[(7 - (arr2[i] % 7)) % 7] = tmp
+        if zero == 6:
+            idx[(7 - ((prefix[i] % 7))) % 7] = tmp # 값 돌리기
+            break
 
-    idx = idx[(7 - (arr2[i] % 7)) % 7:] + idx[:((7 - (arr2[i] % 7)) % 7)]
 
-print(idx)
+for i in range(7):
+    if idx[i] != 0:
+        for j in range(n):
+            if arr[j] % 7 == i:
+                answer.append((arr[j] + (prefix[-1] % (10**9 + 7))) % (10**9 + 7))
 
-cnt = 0
-total = sum(arr2)
-answer = []
-for i in range(n* mx):
-    print(i // 7, ((i // 7) * 7) + (i % 7))
-    if idx[i % 7] != 0 and ((i // 7) * 7) + (i % 7) <= n:
-        idx[i % 7] -= 1
-        answer.append((arr[((i // 7) * 7) + (i % 7)] + total) % (10**9 + 7))
-        cnt += 1
+if answer == []:
+    print(m)
+    print(*arr)
+    exit()
 
-    print(idx)
-
-print(cnt)
+answer.sort()
+print(sum(idx))
 print(*answer)
