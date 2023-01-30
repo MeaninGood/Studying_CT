263b794e3670034e84619ad02d1f5d17a85bb425
JangJiseon <92563854+MeaninGood@users.noreply.github.com>
2023-01-30 22:31:48 +0900

    [Silver II] Title: 연산자 끼워넣기 (2), Time: 324 ms, Memory: 118572 KB -BaekjoonHub

---
+++ 백준/Silver/15658. 연산자 끼워넣기 （2）/연산자 끼워넣기 （2）.py
@@ -0,0 +1,37 @@
+import sys
+input = lambda : sys.stdin.readline().strip()
+
+
+calc = [
+    lambda x, y: x + y,
+    lambda x, y: x - y,
+    lambda x, y: x * y,
+    lambda x, y: x // y if x > 0 else -(abs(x) // y)
+]
+
+def recur(cur, total):
+    global mn, mx
+    
+    if cur == n - 1:
+        mn = min(mn, total)
+        mx = max(mx, total)
+        return
+    
+    for i in range(4):
+        if oper[i] > 0:
+            oper[i] -= 1
+            recur(cur + 1, calc[i](total, arr[cur + 1]))
+            oper[i] += 1
+        
+    
+n = int(input())
+arr = list(map(int, input().split()))
+oper = list(map(int, input().split()))
+
+mn = 10 ** 9 + 1
+mx = -(10 ** 9 + 1)
+
+recur(0, arr[0])
+
+print(mx)
+print(mn)
