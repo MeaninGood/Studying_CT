0e64195aead4033c554e582991c22eb4abd25d2b
MeaninGood <jsmeaningood@gmail.com>
2023-02-05 17:10:45 +0900

    23.02.05:

---
+++ baekjoon/Gold/5_5430.py
@@ -0,0 +1,40 @@
+import sys
+input = lambda : sys.stdin.readline().strip()
+
+T = int(input())
+for tc in range(T):
+    AB = input()
+    n = int(input())
+    arr = eval(input())
+
+    rev = False
+    ldcnt = 0
+    rdcnt = 0
+    for rd in AB:
+        if rd == 'R':
+            rev = not rev
+        else:
+            if ldcnt + rdcnt >= n or n == 0:
+                print('error')
+                break
+
+            if not rev:
+                ldcnt += 1
+            
+            elif rev:
+                rdcnt += 1
+        
+    else:
+        # tmp = []
+        # for i in range(ldcnt, n - rdcnt):
+        #     tmp.append(arr[i])
+
+        # tmp = tmp[::-1] if rev else tmp
+        # print('[' + ','.join(map(str, tmp)) + ']')
+        tmp = ''
+        for i in range(ldcnt, n - rdcnt):
+            tmp += str(arr[i])
+            tmp += ','
+
+        tmp = tmp[:-1] if not rev else tmp[:-1][::-1]
+        tmp = '[' + tmp + ']'
