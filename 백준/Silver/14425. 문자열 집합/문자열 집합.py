b678ea6afee5d466eb09f29c073417f4b7c34990
JangJiseon <92563854+MeaninGood@users.noreply.github.com>
2023-02-13 05:28:29 +0900

    [Silver III] Title: 문자열 집합, Time: 280 ms, Memory: 122108 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,47 +1,16 @@
 import sys
 input = lambda : sys.stdin.readline().strip()
 
-class Trie():
-    
-    def __init__(self):
-        self.root = {}
-        
-        
-    def insert(self, arr):
-        cur_node = self.root
-        
-        for nxt in arr:
-            if nxt not in cur_node:
-                cur_node[nxt] = {}
-            
-            cur_node = cur_node[nxt]
-            
-        cur_node["*"] = {}
-        
-        
-    def search(self, arr):
-        cur_node = self.root
-        
-        for nxt in arr:
-            if nxt in cur_node:
-                cur_node = cur_node[nxt]
-                
-            else:
-                return False
-            
-        return "*" in cur_node
-
-
 n, m = map(int, input().split())
-trie = Trie()
+d = {}
 for i in range(n):
-    tmp = list(input())
-    trie.insert(tmp)
-    
+    tmp = input()
+    d[tmp] = d.get(tmp, 0) + 1
+
 answer = 0
 for i in range(m):
-    tmp = list(input())
-    if trie.search(tmp):
+    tmp = input()
+    if d.get(tmp):
         answer += 1
         
 print(answer)
