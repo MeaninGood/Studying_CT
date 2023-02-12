46f4bc5019748847447907ba1d8eeec67eb74ac1
MeaninGood <jsmeaningood@gmail.com>
2023-02-12 17:14:02 +0900

    23.02.12

---
+++ baekjoon/Gold/5_12904.py
@@ -0,0 +1,24 @@
+import sys
+input = lambda : sys.stdin.readline().strip()
+
+arr1 = list(input())
+arr2 = list(input())
+
+n, m = len(arr1), len(arr2)
+for i in range(m):
+    if arr1 == arr2:
+        print(1)
+        break
+    
+    if len(arr1) >= len(arr2):
+        print(0)
+        break
+    
+    if arr2[-1] == 'A':
+        arr2 = arr2[:-1]
+        
+    elif arr2[-1] == 'B':
+        arr2 = arr2[:-1]
+        arr2 = arr2[::-1]
+        
+    print(arr2)
