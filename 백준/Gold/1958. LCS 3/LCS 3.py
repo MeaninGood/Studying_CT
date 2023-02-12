7468448d1f008e67ad923d7441b1a861bf84c599
JangJiseon <92563854+MeaninGood@users.noreply.github.com>
2023-02-12 17:56:06 +0900

    [Gold III] Title: LCS 3, Time: 312 ms, Memory: 131496 KB -BaekjoonHub

---
+++ 백준/Gold/1958. LCS 3/LCS 3.py
@@ -0,0 +1,25 @@
+import sys
+input = lambda : sys.stdin.readline().strip()
+from pprint import pprint
+
+arr1 = ['#'] + list(input())
+arr2 = ['#'] + list(input())
+arr3 = ['#'] + list(input())
+
+n, m, z = len(arr1), len(arr2), len(arr3)
+
+arr = [[[0 for i in range(z)] for j in range(m)] for k in range(n)]
+
+answer = 0
+for i in range(1, n):
+    for j in range(1, m):
+        for k in range(1, z):
+            if arr1[i] == arr2[j] and arr2[j] == arr3[k]:
+                arr[i][j][k] = arr[i - 1][j - 1][k - 1] + 1
+                
+            else:
+                arr[i][j][k] = max(arr[i - 1][j][k], arr[i][j - 1][k], arr[i][j][k - 1])
+            
+            answer = max(answer, arr[i][j][k])
+
+print(answer)
