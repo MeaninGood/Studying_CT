856ce0780a01ca47f18037712a6fa1996a5b07f8
MeaninGood <jsmeaningood@gmail.com>
2023-02-13 21:53:14 +0900

    23.02.13

---
+++ baekjoon/Platinum/5_9202.py
@@ -0,0 +1,71 @@
+import sys
+from collections import deque
+input = lambda : sys.stdin.readline().strip()
+
+
+class Trie():
+    
+    def __init__(self):
+        self.root = {}
+        
+    
+    def insert(self, arr):
+        cur_node = self.root
+        
+        for nxt in arr:
+            if nxt not in cur_node:
+                cur_node[nxt] = {}
+                
+            cur_node = cur_node[nxt]
+            
+    
+    def search(self, arr):
+        cur_node = self.root
+        
+        for nxt in arr:
+            if nxt in cur_node:
+                cur_node = cur_node[nxt]
+            
+            else:
+                return False
+            
+        return "*" in cur_node
+
+
+def in_range(x, y):
+    return 0 <= x < 4 and 0 <= y < 4
+
+
+dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+def bfs(x, y):
+    visited = [[False for _ in range(4)] for _ in range(4)]
+    que = deque()
+    
+    visited[x][y] = True
+    que.append([x, y])
+    
+    while que:
+        size = len(que)
+        
+        for _ in range(size):
+            x, y = que.popleft()
+            
+            for i in range(8):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                
+                if not in_range(nx, ny) or not trie.search
+
+trie = Trie()
+n = int(input())
+for _ in range(n):
+    tmp = input()
+    trie.insert(tmp)
+    
+x = input()
+m = int(input())
+for _ in range(m):
+    arr = [list(input()) for _ in range(4)]
+    
+    x = input()
---
+++ baekjoon/Silver/3_14425.py
@@ -0,0 +1,65 @@
+import sys
+input = lambda : sys.stdin.readline().strip()
+
+class Trie():
+    
+    def __init__(self):
+        self.root = {}
+        
+        
+    def insert(self, arr):
+        cur_node = self.root
+        
+        for nxt in arr:
+            if nxt not in cur_node:
+                cur_node[nxt] = {}
+            
+            cur_node = cur_node[nxt]
+            
+        cur_node["*"] = {}
+        
+        
+    def search(self, arr):
+        cur_node = self.root
+        
+        for nxt in arr:
+            if nxt in cur_node:
+                cur_node = cur_node[nxt]
+                
+            else:
+                return False
+            
+        return "*" in cur_node
+
+
+n, m = map(int, input().split())
+trie = Trie()
+for i in range(n):
+    tmp = list(input())
+    trie.insert(tmp)
+    
+answer = 0
+for i in range(m):
+    tmp = list(input())
+    if trie.search(tmp):
+        answer += 1
+        
+print(answer)
+
+
+import sys
+input = lambda : sys.stdin.readline().strip()
+
+n, m = map(int, input().split())
+d = {}
+for i in range(n):
+    tmp = input()
+    d[tmp] = d.get(tmp, 0) + 1
+
+answer = 0
+for i in range(m):
+    tmp = input()
+    if d.get(tmp):
+        answer += 1
+        
+print(answer)
