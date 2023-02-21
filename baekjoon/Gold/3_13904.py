1db356eb3b0d3ed2e01de5ac711a3738b02958af
MeaninGood <jsmeaningood@gmail.com>
2023-02-22 00:01:28 +0900

    23.02.22

--- baekjoon/Gold/3_13904.py
+++ baekjoon/Gold/3_13904.py
@@ -17,32 +17,5 @@ while pq:
         
         d[i] = d.get(i, -score)
         break
-    
-print(d)
+
 print(sum(d.values()))
-    
-    
-"""
-n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-
-arr.sort(key = lambda x: -x[1])
-
-# print(arr)
-# idx = 0
-# total = 0
-# cnt = 0
-# while arr:
-#     if arr[0][0] > idx and cnt <= idx:
-#         idx = arr[0][0]
-#         total += arr[0][1]
-#         cnt += 1
-        
-#     elif arr[0][0] <= idx and cnt < idx:
-#         total += arr[0][1]
-#         cnt += 1
-
-#     arr.pop(0)
-    
-# print(total)
-"""
