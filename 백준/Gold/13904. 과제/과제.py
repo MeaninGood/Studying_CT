8018a2a6e220eb5c46d5fd9bd54fd0b5318809a5
JangJiseon <92563854+MeaninGood@users.noreply.github.com>
2023-02-21 23:59:45 +0900

    [Gold III] Title: 과제, Time: 148 ms, Memory: 115280 KB -BaekjoonHub

---
+++ 백준/Gold/13904. 과제/과제.py
@@ -0,0 +1,21 @@
+import sys
+import heapq
+input = lambda : sys.stdin.readline().strip()
+
+n = int(input())
+pq = []
+for i in range(n):
+    a, b = map(int, input().split())
+    heapq.heappush(pq, (-b, a))
+
+d = {}
+while pq:
+    score, day = heapq.heappop(pq)
+    for i in range(1, day + 1)[::-1]:
+        if d.get(i):
+            continue
+        
+        d[i] = d.get(i, -score)
+        break
+    
+print(sum(d.values()))
