eeb82b51865b044b98c21561bc31e6fa06412820
MeaninGood <jsmeaningood@gmail.com>
2023-03-27 20:46:00 +0900

    23.03.27

--- grammar/btrk2.py
+++ grammar/btrk2.py
@@ -121,10 +121,10 @@ input = lambda : sys.stdin.readline().strip()
 # 4번 템플릿
 # 도영이 문제 3번 템플릿으로 풀어보고, 4번 왜 쓰는지 알려줌
 ## 3번 템플릿 - 2번 템플릿도 되긴 하는데 시간이 오래 걸림
-n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
+# n = int(input())
+# arr = [list(map(int, input().split())) for _ in range(n)]
 
-ans = 1 << 60
+# ans = 1 << 60
 # 하나만 고르는 것도 보고, 2, 3개 고르는 것 등 길이가 정해져 있지 않음 - length 정하기
 # def recur(cur, start, tmp):
 #     if cur == length:
@@ -136,20 +136,64 @@ ans = 1 << 60
 
 
 # 3번 템플릿 응용
-def recur(cur, start, a, b):
+# def recur(cur, start, a, b):
+#     global ans
+
+#     if cur == length:
+#         ans = min(ans, abs(a - b))
+#         return
+    
+#     for i in range(start, n):
+#         recur(cur + 1, i + 1, a * arr[i][0], b + arr[i][1])
+
+
+# for i in range(1, n + 1):
+#     length = i
+#     # recur(0, 0, [])
+#     recur(0, 0, 1, 0)
+
+# print(ans)
+
+
+
+## 4번 템플릿
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+ans = 1 << 60
+
+def recur(cur, cnt, a, b):
     global ans
 
-    if cur == length:
+    # 몇 개를 고르든 다 보겠다
+    if cnt != 0:
         ans = min(ans, abs(a - b))
+
+    # O를 내가 원하는 개수만큼 골랐으면 조기 종료
+    # if cnt == length: # 충분히 골랐으면 return하겠다
+    #     ans = min(ans, abs(a - b))
+    #     return
+    
+    if cur == n: # 충분히 못 골랐는데 끝까지 왔다
         return
     
-    for i in range(start, n):
-        recur(cur + 1, i + 1, a * arr[i][0], b + arr[i][1])
+    # 반복문이 없으니 cur이 들어감
+    # 하나의 함수에서 두개씩 호출
+    recur(cur + 1, cnt + 1, a * arr[cur][0], b + arr[cur][1]) # 고르겠다
+    recur(cur + 1, cnt, a, b) # 안 고르겠다
 
 
-for i in range(1, n + 1):
-    length = i
-    # recur(0, 0, [])
-    recur(0, 0, 1, 0)
+# for i in range(1, n + 1):
+#     length = i
+#     recur(0, 0, 1, 0)
 
-print(ans)
+recur(0, 0, 1, 0)
+
+
+# 4번 몰라도 3번으로도 풀리긴 함
+# 근데 4번이 훨씬 더 직관적임.
+# 일반적으로 알려주는 이유는 2차원 백트래킹을 풀기 위해
+# 이거는 1차원이고
+# 퇴사는 쉬우니까 그냥 푸는 걸로
+
+
