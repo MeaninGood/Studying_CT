6149f14e15d8d7b0a02ce2949b078d48ef3146f4
JangJiseon <92563854+MeaninGood@users.noreply.github.com>
2024-01-04 03:17:38 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 124 ms, Memory: 110556 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -3,19 +3,21 @@ a, b, c, d, e, f = map(int, input().split(' '))
 def getResult(x, y):
     return a * x + b * y == c and d * x + e * y == f
 
-ans = []
 for i in range(1000):
     for j in range(1000):
         if getResult(i, j):
-            ans.append([i, j])
+            print(i, j)
+            break
             
         if getResult(-i, j):
-            ans.append([-i, j])
+            print(-i, j)
+            break
             
         if getResult(i, -j):
-            ans.append([i, -j])
+            print(i, -j)
+            break
             
         if getResult(-i, -j):
-            ans.append([-i, -j])
+            print(-i, -j)
+            break
             
-print(*ans[0])
