77b8bb93f89381caf3a765247b88a17cafeaa0cd
JangJiseon <92563854+MeaninGood@users.noreply.github.com>
2024-01-04 14:52:15 +0900

    [Bronze II] Title: 방 배정하기, Time: 120 ms, Memory: 109240 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.py
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.py
@@ -1,17 +1,12 @@
 a, b, c, n = map(int, input().split(' '))
 
-arr = sorted([a, b, c], reverse=True)
-a, b, c = arr[0], arr[1], arr[2]
-
 for i in range(300):
-    if a * i > n:
-        break
-    
     for j in range(300):
-        if (b * j) > n:
-            break
 
-        if (n - (a * i) + (b * j)) % c == 0:
+        if (a * i) + (b * j) > n:
+            break
+        
+        if (n - (a * i) - (b * j)) % c == 0:
             print(1)
             exit()
             
