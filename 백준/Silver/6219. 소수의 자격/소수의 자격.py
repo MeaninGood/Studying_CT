70d0dab20d413a61a8c2cee4f7e20a4aa7479c3c
JangJiseon <92563854+MeaninGood@users.noreply.github.com>
2024-01-11 22:44:27 +0900

    [Silver III] Title: 소수의 자격, Time: 1204 ms, Memory: 141688 KB -BaekjoonHub

---
+++ 백준/Silver/6219. 소수의 자격/소수의 자격.py
@@ -0,0 +1,20 @@
+import sys
+input = lambda : sys.stdin.readline().strip()
+
+a, b, d = map(int, input().split(' '))
+
+sieve = [True for _ in range(b + 1)]
+sieve[0], sieve[1] = False, False 
+for i in range(2, b + 1):
+    if i * i > b + 1:
+        break
+    
+    for j in range(i * i, b + 1, i):
+        sieve[j] = False
+        
+cnt = 0
+for i in range(a, b + 1):
+    if str(d) in str(i) and sieve[i] == True:
+        cnt += 1
+        
+print(cnt)
