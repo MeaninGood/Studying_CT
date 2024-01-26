6fc1b00fa5b7b9746e8d8a6fa5c2740f293337e6
JangJiseon <92563854+MeaninGood@users.noreply.github.com>
2024-01-26 23:20:36 +0900

    [Silver II] Title: 창고 다각형, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -1,16 +1,19 @@
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+
 n = int(input())
 arr = [0 for _ in range(1010)]
 for _ in range(n):
-    a, b = map(int, input().split(' '))
-    
+    a, b = map(int, input().split())
     arr[a] = b
     
-prefix = [0 for i in range(1010)]
+prefix = [0 for _ in range(1010)]
 for i in range(1, 1010):
     prefix[i] = max(prefix[i - 1], arr[i])
     
-suffix = [0 for i in range(1010)]
-for i in range(1009)[::-1]:
+suffix = [0 for _ in range(1010)]
+for i in range(1001)[::-1]:
     suffix[i] = max(suffix[i + 1], arr[i])
     
 ans = 0
