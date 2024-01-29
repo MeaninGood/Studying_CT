80f3bbf5a4acc2396e33c033dc820d59fcf6e705
MeaninGood <jsmeaningood@gmail.com>
2024-01-29 23:48:13 +0900

    24.01.29

--- baekjoon/Gold/3_2143.py
+++ baekjoon/Gold/3_2143.py
@@ -9,32 +9,21 @@ m = int(input())
 barr = list(map(int, input().split()))
 
 prefixa = [aarr[0] for _ in range(n)]
-ad = {aarr[0]: 1}
+d = {aarr[0]: 1}
 for i in range(1, n):
     prefixa[i] = prefixa[i - 1] + aarr[i]
-    ad[prefixa[i]] = ad.get(prefixa[i], 0) + 1
-    ad[aarr[i]] = ad.get(aarr[i], 0) + 1
-    
+    d[prefixa[i]] = d.get(prefixa[i], 0) + 1
+    for j in range(i):
+        tmp = prefixa[i] - prefixa[j]
+        d[tmp] = d.get(tmp, 0) + 1
 
+ans = d.get(k - barr[0], 0)
 prefixb = [barr[0] for _ in range(m)]
-bd = {barr[0]: 1}    
 for i in range(1, m):
     prefixb[i] = prefixb[i - 1] + barr[i]
-    bd[prefixb[i]] = bd.get(prefixb[i], 0) + 1
-    bd[barr[i]] = bd.get(barr[i], 0) + 1
-    
-print(ad)
-print(bd)
-ans = 0
-for i in range(n):
-    if prefixa[i] == k:
-        ans += 1
-        
-    target = prefixa[i] - k
-    ans += bd.get(target, 0)
-        
-for i in range(m):
-    target = prefixb[i] - k
-    ans += ad.get(target, 0)
-    
+    ans += d.get(k - prefixb[i], 0)
+    for j in range(i):
+        tmp = prefixb[i] - prefixb[j]
+        ans += d.get(k - tmp, 0)
+print(d)
 print(ans)
--- baekjoon/Gold/3_24525.py
+++ baekjoon/Gold/3_24525.py
@@ -1,84 +1,120 @@
+# arr = list(input())
+# n = len(arr)
+# sarr = [0 for _ in range(n)]
+# sarr[0] += arr[0] == 'S'
+# for i in range(1, n):
+#     sarr[i] = sarr[i - 1] + (arr[i] == 'S')
+# sarr + [0]
+
+# karr = [0 for _ in range(n)]
+# for i in range(n - 1)[::-1]:
+#     karr[i] = karr[i + 1] + (arr[i] == 'K')
+    
+# s, e = n - 1, 0
+# mx = -1
+# print(sarr)
+# print(karr)
+# while s >= 0 and e < n - 1:
+#     if sarr[s] != 0 and karr[e] == sarr[s] * 2:
+#         tmps = sarr[s]
+#         while sarr[s] == tmps and s >= 0:
+#             s -= 1
+            
+#         tmpk = karr[e]
+#         while karr[e] == tmpk and e < n - 1:
+#             e += 1
+#         mx = max(abs(s - e), mx)
+        
+#     if karr[e] == sarr[s]:
+#         tmps = sarr[s]
+#         while sarr[s] == tmps and s >= 0:
+#             s -= 1
+            
+#         tmpk = karr[e]
+#         while karr[e] == tmpk and e < n - 1:
+#             e += 1
+            
+#     elif sarr[s] > karr[e]:
+#         e += 1
+        
+#     else:
+#         s -= 1
+    
+# print(mx)
+
+# import sys
+
+# input = lambda: sys.stdin.readline().strip()
+
+# def check(a, b):
+#     return a != 0 and b != 0
+
+
+# arr = list(input())
+# n = len(arr)
+
+# prefixs = [0 for _ in range(n)]
+# prefixs[0] += arr[0] == "S"
+# prefixk = [0 for _ in range(n)]
+# prefixk[0] += arr[0] == "K"
+# for i in range(1, n):
+#     prefixs[i] = prefixs[i - 1] + (arr[i] == "S")
+#     prefixk[i] = prefixk[i - 1] + (arr[i] == "K")
+
+# suffixs = [0 for _ in range(n)]
+# suffixs[-1] += arr[-1] == "S"
+# suffixk = [0 for _ in range(n)]
+# suffixk[-1] += arr[-1] == "K"
+# for i in range(n - 1)[::-1]:
+#     suffixs[i] = suffixs[i + 1] + (arr[i] == "S")
+#     suffixk[i] = suffixk[i + 1] + (arr[i] == "K")
+
+
+# mx = -1
+# for i in range(n):
+#     a, b = prefixs[i], prefixk[i]
+#     if check(a, b) and a * 2 == b:
+#         mx = max(mx, i + 1)
+
+# for i in range(n - 1):
+#     a, b = suffixs[i], suffixk[i]
+#     if check(a, b) and a * 2 == b:
+#         mx = max(mx, n - i)
+
+# print(mx)
+
+
 import sys
 
 input = lambda: sys.stdin.readline().strip()
 
-arr = list(input())
-n = len(arr)
-sarr = [0 for _ in range(n)]
-sarr[0] += arr[0] == 'S'
-for i in range(1, n):
-    sarr[i] = sarr[i - 1] + (arr[i] == 'S')
-sarr + [0]
-
-karr = [0 for _ in range(n)]
-for i in range(n - 1)[::-1]:
-    karr[i] = karr[i + 1] + (arr[i] == 'K')
-    
-s, e = n - 1, 0
-mx = -1
-print(sarr)
-print(karr)
-while s >= 0 and e < n - 1:
-    if sarr[s] != 0 and karr[e] == sarr[s] * 2:
-        tmps = sarr[s]
-        while sarr[s] == tmps and s >= 0:
-            s -= 1
-            
-        tmpk = karr[e]
-        while karr[e] == tmpk and e < n - 1:
-            e += 1
-        mx = max(abs(s - e), mx)
-        
-    if karr[e] == sarr[s]:
-        tmps = sarr[s]
-        while sarr[s] == tmps and s >= 0:
-            s -= 1
-            
-        tmpk = karr[e]
-        while karr[e] == tmpk and e < n - 1:
-            e += 1
-            
-    elif sarr[s] > karr[e]:
-        e += 1
-        
-    else:
-        s -= 1
-    
-print(mx)
-
 def check(a, b):
     return a != 0 and b != 0
 
+def getScore(a):
+    if a == 'S':
+        return 2
+    
+    elif a == 'K':
+        return -1
+    
+    return 0
 
 arr = list(input())
 n = len(arr)
 
-prefixs = [0 for _ in range(n)]
-prefixs[0] += arr[0] == "S"
-prefixk = [0 for _ in range(n)]
-prefixk[0] += arr[0] == "K"
+prefix = [0 for _ in range(n)]
+prefix[0] += getScore(arr[0])
 for i in range(1, n):
-    prefixs[i] = prefixs[i - 1] + (arr[i] == "S")
-    prefixk[i] = prefixk[i - 1] + (arr[i] == "K")
+    tmp = getScore(arr[i])
+    prefix[i] = prefix[i - 1] + tmp
 
-suffixs = [0 for _ in range(n)]
-suffixs[-1] += arr[-1] == "S"
-suffixk = [0 for _ in range(n)]
-suffixk[-1] += arr[-1] == "K"
+
+suffix = [0 for _ in range(n)]
+suffix[-1] += getScore(arr[-1])
 for i in range(n - 1)[::-1]:
-    suffixs[i] = suffixs[i + 1] + (arr[i] == "S")
-    suffixk[i] = suffixk[i + 1] + (arr[i] == "K")
-
-
-mx = -1
-for i in range(n):
-    a, b = prefixs[i], prefixk[i]
-    if check(a, b) and a * 2 == b:
-        mx = max(mx, i + 1)
-
-for i in range(n - 1):
-    a, b = suffixs[i], suffixk[i]
-    if check(a, b) and a * 2 == b:
-        mx = max(mx, n - i)
-
-print(mx)
+    tmp = getScore(arr[i])
+    suffix[i] = suffix[i + 1] + tmp
+    
+print(prefix)
+print(suffix)
