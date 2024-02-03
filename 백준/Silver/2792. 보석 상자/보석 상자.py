958dfebc5d19da932aafe08025436872a3171faf
JangJiseon <92563854+MeaninGood@users.noreply.github.com>
2024-02-03 16:26:40 +0900

    [Silver I] Title: 보석 상자, Time: 404 ms, Memory: 112768 KB -BaekjoonHub

--- 백준/Silver/2792. 보석 상자/보석 상자.py
+++ 백준/Silver/2792. 보석 상자/보석 상자.py
@@ -1,32 +1,32 @@
-n, m = map(int, input().split())
- 
-arr = []
-for i in range(m):
-    arr.append(int(input()))
+import sys
 
-arr.sort()
+input = lambda: sys.stdin.readline().strip()
+
+n, m = map(int, input().split())
+arr = [int(input()) for _ in range(m)]
 
 def check(x):
     total = 0
-    for i in range(len(arr)):
+    
+    for i in range(m):
         total += arr[i] // x
         
         if arr[i] % x:
             total += 1
-
+            
     return total <= n
 
-s = 1
-e = 100000000000
-idx = 0
+
+s, e = 1, 1 << 31
+ans = 0
 while s <= e:
     mid = (s + e) // 2
     
     if check(mid):
-         idx = mid
-         e = mid - 1
-         
+        ans = mid
+        e = mid - 1
+        
     else:
         s = mid + 1
-
-print(idx)
+        
+print(ans)
