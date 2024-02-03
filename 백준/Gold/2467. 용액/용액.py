8b8bf72f9d478e5704505d246dc46b2e3839e99d
JangJiseon <92563854+MeaninGood@users.noreply.github.com>
2024-02-03 21:47:41 +0900

    [Gold V] Title: 용액, Time: 148 ms, Memory: 126196 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -1,17 +1,17 @@
 import sys
 
-input = lambda : sys.stdin.readline().strip()
+input = lambda: sys.stdin.readline().strip()
 
 n = int(input())
 arr = list(map(int, input().split()))
 
 s, e = 0, n - 1
-mn = 1 << 31
-x, y = arr[s], arr[e]
+x, y = arr[0], arr[n - 1]
 while s < e:
-    total = arr[e] + arr[s]
-    if abs(total) < mn:
-        mn = abs(total)
+    target = x + y
+    total = arr[s] + arr[e]
+    
+    if abs(total) < abs(target):
         x, y = arr[s], arr[e]
         
     elif total < 0:
