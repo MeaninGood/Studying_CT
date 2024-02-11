8cfa9ce14b9404caabb23a63f0a1a6aeef9ff129
JangJiseon <92563854+MeaninGood@users.noreply.github.com>
2024-02-11 23:35:24 +0900

    [Silver I] Title: 나무꾼 이다솜, Time: 124 ms, Memory: 110404 KB -BaekjoonHub

--- 백준/Silver/1421. 나무꾼 이다솜/나무꾼 이다솜.py
+++ 백준/Silver/1421. 나무꾼 이다솜/나무꾼 이다솜.py
@@ -1,21 +1,23 @@
 import sys
-input = lambda : sys.stdin.readline().strip()
+
+input = lambda: sys.stdin.readline().strip()
 
 n, c, w = map(int, input().split())
-arr = [int(input()) for _ in range(n)]
+arr = [int(input())for _ in range(n)]
 arr.sort()
-ans = 0
-cnt = arr[-1]
 
-while cnt > 0:
+ans = 0
+cnt = 1
+
+while cnt <= arr[-1]:
     total = 0
     for tree in arr:
         tmp = (cnt * (tree // cnt) * w) - (c * ((tree // cnt) - ((tree % cnt) == 0)))
-        if tmp < 0:
-            continue
-        total += tmp
-
-    ans = max(total, ans)
-    cnt -= 1
+        
+        if tmp > 0:
+            total += tmp
+        
+    ans = max(ans, total)
+    cnt += 1
     
 print(ans)
