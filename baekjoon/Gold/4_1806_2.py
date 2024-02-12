f734bd9b5df75259769527241b63235772945833
MeaninGood <jsmeaningood@gmail.com>
2024-02-12 14:19:03 +0900

    Merge branch 'main' of https://github.com/MeaninGood/Studying_CT

--- baekjoon/Gold/4_1806_2.py
+++ baekjoon/Gold/4_1806_2.py
@@ -1,29 +1,8 @@
 import sys
-<<<<<<< HEAD
-=======
-
->>>>>>> 95d4d62deb7b7e8eeb97d9b665731a90f2865bdc
 input = lambda : sys.stdin.readline().strip()
 
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
-<<<<<<< HEAD
-
-s, e = 0, 0
-total = arr[0]
-cnt = 1
-mn = 1 << 31
-
-while e < n:
-    if total < k:
-        cnt += 1
-        e += 1
-        total += arr[e]
-
-    elif total >= k:
-        mn = min(cnt, mn)
-        
-=======
 arr += [0]
 
 s, e = 0, 0 
@@ -46,4 +25,3 @@ while e < n:
         
 
 print(ans if cnt else 0)
->>>>>>> 95d4d62deb7b7e8eeb97d9b665731a90f2865bdc
