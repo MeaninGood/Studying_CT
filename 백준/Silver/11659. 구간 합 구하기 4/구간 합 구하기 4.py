1a3e7aa2ef3dfe702a63656ccdcf8eb3d21362bf
JangJiseon <92563854+MeaninGood@users.noreply.github.com>
2024-10-31 00:55:31 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 180 ms, Memory: 122956 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,16 +1,17 @@
 import sys
-
-input = lambda: sys.stdin.readline().strip()
+input = lambda : sys.stdin.readline().strip()
 
 n, m = map(int, input().split())
 arr = [0] + list(map(int, input().split()))
 
 prefix = [0 for _ in range(n + 1)]
-
 for i in range(1, n + 1):
     prefix[i] = prefix[i - 1] + arr[i]
     
+
 for i in range(m):
     a, b = map(int, input().split())
     
-    print(prefix[b] - prefix[a - 1])
+    ans = prefix[b] - prefix[a - 1]
+    
+    print(ans)
