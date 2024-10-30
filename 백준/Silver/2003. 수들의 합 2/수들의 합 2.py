23c2ce7b3188ae1485a2f96d3c2c7c1582f48227
JangJiseon <92563854+MeaninGood@users.noreply.github.com>
2024-10-31 00:50:01 +0900

    [Silver IV] Title: 수들의 합 2, Time: 100 ms, Memory: 110552 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -1,21 +1,20 @@
 import sys
-
 input = lambda : sys.stdin.readline().strip()
+
 
 n, m = map(int, input().split())
 arr = list(map(int, input().split())) + [0]
 
 s, e = 0, 0
+ans = 0
 total = arr[0]
-cnt = 0
 while e < n:
     if total == m:
-        cnt += 1
-        total -= arr[s]
-        s += 1
+        ans += 1
+        
         e += 1
         total += arr[e]
-
+        
     elif total < m:
         e += 1
         total += arr[e]
@@ -23,5 +22,6 @@ while e < n:
     else:
         total -= arr[s]
         s += 1
+
         
-print(cnt)
+print(ans)
