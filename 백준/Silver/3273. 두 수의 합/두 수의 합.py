f0ad876af8bafd7ae4d3ad403c11cea91b7ad74e
JangJiseon <92563854+MeaninGood@users.noreply.github.com>
2024-10-31 00:34:33 +0900

    [Silver III] Title: 두 수의 합, Time: 120 ms, Memory: 121564 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,23 +1,25 @@
 import sys
+input = lambda: sys.stdin.readline().strip()
 
-input = lambda : sys.stdin.readline().strip()
-
-n= int(input())
-arr = sorted(list(map(int, input().split())))
+n = int(input())
+arr = list(map(int, input().split()))
 x = int(input())
 
+arr.sort()
+
+
 s, e = 0, n - 1
-cnt = 0
+
+ans = 0
 while s < e:
     if arr[s] + arr[e] == x:
-        cnt += 1
+        ans += 1
         s += 1
-        e -= 1
-        
+
     elif arr[s] + arr[e] < x:
         s += 1
         
     else:
         e -= 1
         
-print(cnt)
+print(ans)
