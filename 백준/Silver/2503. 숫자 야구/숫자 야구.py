5336d13fb4faa507c00bffef6b69e91bcb7f5368
JangJiseon <92563854+MeaninGood@users.noreply.github.com>
2024-10-31 00:22:44 +0900

    [Silver III] Title: 숫자 야구, Time: 100 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,29 +1,47 @@
-n = int(input())
-arr = [list(map(int, input().split(' '))) for _ in range(n)]
+import sys
+input = lambda : sys.stdin.readline().strip()
 
-nums = []
+arr = []
+
 for i in range(1, 10):
     for j in range(1, 10):
         for k in range(1, 10):
-            if i == j or j == k or i == k:
+            if i == j or i == k or j == k:
                 continue
             
-            nums.append([str(i), str(j), str(k)])
+            arr.append(str(i) + str(j) + str(k))
+            
 
-ans = nums[:]
-for target, stk, ball in arr:
-    tmp = list(str(target))
+n = int(input())
+m = len(arr)
+
+visited = [True for _ in range(m)]
+for i in range(n):
+    ans, strk, ball = map(int, input().split())
     
-    for num in nums:
-        nstk, nball = 0, 0
+    a, b, c = str(ans)[0], str(ans)[1], str(ans)[2]
+    
+    for j in range(m):
+        if not visited[j]:
+            continue
         
-        for i in range(3):
-            if num[i] == tmp[i]:
-                nstk += 1
-            elif tmp[i] in num:
-                nball += 1
-                
-        if (nstk != stk or nball != ball) and num in ans:
-            ans.remove(num)
-
-print(len(ans))
+        scnt, bcnt = 0, 0
+        
+        x, y, z = arr[j][0], arr[j][1], arr[j][2]
+        
+        # strk 체크
+        scnt += (a == x) + (b == y) + (c == z)
+        
+        # ball 체크
+        bcnt += (a == y) + (a == z) + (b == x) + (b == z) + (c == x) + (c == y)
+            
+        if scnt != strk or bcnt != ball:
+            visited[j] = False
+            
+    
+ans = 0        
+for i in range(m):
+    if visited[i]:
+        ans += 1
+        
+print(ans)
